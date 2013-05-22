3452bf0dd1b9a3e0632f47f1ed8775dd7f588c31
Patrick <glafodiase+git@gmail.com>
2013-05-22 18:17:41 -0400

    initial setup

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Patrick
+ * Date: 22/05/13
+ * Time: 6:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+
+
+    void main()
+    {
+
+
+    }
+}
